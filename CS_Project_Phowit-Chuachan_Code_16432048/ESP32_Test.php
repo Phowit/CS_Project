fe37f388169d4ad7dde37db080aefb979fb384eb
DESKTOP-TJ66K5G\Le380 <phowit26@gmail.com>
2025-09-09 00:01:32 +0700

    don't want this file.

--- CS_Project_Phowit-Chuachan_Code_16432048/ESP32_Test.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['data'])) { // รับค่าที่ส่งมาด้วย method GET
-    $data = $_GET['data']; // รับค่าพารามิเตอร์ชื่อ 'data'
-    echo "Received data: " . $data; // แสดงข้อมูลที่ได้รับ
-} else {
-    echo "No data received"; // หากไม่มีข้อมูลที่ส่งมา
-}
-?>
