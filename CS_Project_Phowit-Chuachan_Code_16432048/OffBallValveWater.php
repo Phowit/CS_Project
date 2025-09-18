f9d6a084a05a4aaf52b08c8b04bd64b7ec676922
DESKTOP-TJ66K5G\Le380 <phowit26@gmail.com>
2025-09-18 22:04:48 +0700

    on-off BV tem and water can chang status. but it can't out put data to board.

---
+++ CS_Project_Phowit-Chuachan_Code_16432048/OffBallValveWater.php
@@ -0,0 +1,16 @@
+<?php
+// เชื่อมต่อฐานข้อมูล
+require_once("connect_db.php");
+
+// เตรียมคำสั่ง SQL clone row ล่าสุด → เป็น row ใหม่ แต่เปลี่ยนเฉพาะค่า DC_BV_water
+$OffBV_water = "INSERT INTO datacontrol (`Temperature_range`,`TimeFoodS`,`DC_Motor`,`DC_BV_Tem`,`DC_BV_water`,`DC_BV_FoodS`,`User_ID`)
+            SELECT `Temperature_range`,`TimeFoodS`,DC_Motor, `DC_BV_Tem`,0,`DC_BV_FoodS`,`User_ID`
+            FROM datacontrol
+            ORDER BY `DateControl_ID` DESC LIMIT 1
+            ";
+
+mysqli_query($conn,$OffBV_water);
+
+$conn->close();
+?>
+<meta http-equiv="refresh" content = "0; url = Admin_ManageEnvironment.php">
---
+++ CS_Project_Phowit-Chuachan_Code_16432048/OnBallValveFoodS.php
@@ -0,0 +1,16 @@
+<?php
+// เชื่อมต่อฐานข้อมูล
+require_once("connect_db.php");
+
+// เตรียมคำสั่ง SQL clone row ล่าสุด → เป็น row ใหม่ แต่เปลี่ยนเฉพาะค่า DC_BV_water
+$OnBV_FoodS = "INSERT INTO datacontrol (`Temperature_range`,`TimeFoodS`,`DC_Motor`,`DC_BV_Tem`,`DC_BV_water`,`DC_BV_FoodS`,`User_ID`)
+            SELECT `Temperature_range`,`TimeFoodS`,DC_Motor,DC_BV_Tem,DC_BV_water,1,`User_ID`
+            FROM datacontrol
+            ORDER BY `DateControl_ID` DESC LIMIT 1
+            ";
+
+mysqli_query($conn,$OnBV_FoodS);
+
+$conn->close();
+?>
+<meta http-equiv="refresh" content = "0; url = Admin_ManageEnvironment.php">
